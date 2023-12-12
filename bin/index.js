cd21cfa08304658e27d8c7404152560a0a881244
anurag-ay <ay1909069@gmail.com>
2023-12-12 19:30:46 +0530

    update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,33 @@
 #!/usr/bin/env node
-
+import chalk from "chalk";
 import explainCode from "../openAi/explainCode.js";
 import readFile from "../utils/readFile.js";
-import { createSpinner } from "nanospinner";
-import chalkAnimation from "chalk-animation";
+import { startLoaderAnimation, stopLoaderAnimation } from "../utils/loader.js";
 
 //Get the file name from the command line argument
 const fileName = process.argv[2];
 
 async function getExpalinedCode(fileName) {
-  const spinner = createSpinner("Getting explaination please wait....\n");
+  // starting animation while loading
+  const animationObjects = startLoaderAnimation();
+
   try {
+    // Reading file
     const textFileData = await readFile(fileName);
-    spinner.start();
-    // const expalinedCodeText = await explainCode(textFileData);
-    const expalinedCodeText = "Hello My Name is Anurag Yadav ";
-    spinner.success();
+
+    // Passing file's text data for getting explaination
+    const expalinedCodeText = await explainCode(textFileData);
+
+    // stopping loader animation
+    stopLoaderAnimation(animationObjects);
+
+    // Printing code expalaination
     console.log(`${expalinedCodeText}\n`);
+
+    // Print final message
+    console.log(chalk.bgGreen.bold("Hope that helps."));
   } catch (err) {
-    spinner.error({ text: err.message });
+    animationObjects.spinner.error({ text: err.message });
     process.exit(1);
   }
 }
