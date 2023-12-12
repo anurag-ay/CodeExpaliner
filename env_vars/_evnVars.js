e121642323a49ea1e2d0e6b6e145684152da201a
anurag-ay <ay1909069@gmail.com>
2023-12-12 22:05:17 +0530

    update envVars

--- env_vars/_evnVars.js
+++ env_vars/_evnVars.js
@@ -1,3 +1,4 @@
+// Note this key is only for testing purpose and it will be removed after 1 Jan 2024
 const OPENAI_SECRET_KEY = "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";
 
 export default OPENAI_SECRET_KEY;
